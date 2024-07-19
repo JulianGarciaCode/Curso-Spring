5ef32f0db4cb8a8f2be76cc456d933c5ee2da33b
JulianGarciaCode <juliangarciacode@gmail.com>
2024-07-19 04:28:09 +0200

    Modificacion proyecto Spring Web MVC

--- 02.Spring Web MVC/src/main/java/com/example/springwebmvc/controller/ProductController.java
+++ 02.Spring Web MVC/src/main/java/com/example/springwebmvc/controller/ProductController.java
@@ -1,10 +1,15 @@
 package com.example.springwebmvc.controller;
 
+import com.example.springwebmvc.entity.Product;
 import com.example.springwebmvc.repository.ProductRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/products")
@@ -16,13 +21,49 @@ public class ProductController {
     public ProductController(ProductRepository repository) {
         this.repository = repository;
     }
+
     /*
     GET http://localhost:8080/products
      */
     @GetMapping
     public String findAll(Model model){
-        model.addAttribute("product", "Hola mundo");
-        return "product-list";
+
+        List<Product> products = this.repository.findAll();
+        model.addAttribute("products", products);
+        return "product list";
     }
 
+    /*
+    GET http://localhost:8080/products/new
+     */
+    @GetMapping("/new")
+    public String getForm(Model model){
+        model.addAttribute("products", new Product());
+        return "product form";
+    }
+
+    /*
+    POST http://localhost:8080/products
+     */
+    @PostMapping
+    public String save(@ModelAttribute("product") Product product){
+        this.repository.save(product);
+        return "redirect:/products";
+    }
+
+    /*
+    GET http://localhost:8080/products/{id}/view
+     */
+
+    /*
+    GET http://localhost:8080/products/{id}/edit
+     */
+
+    /*
+    GET http://localhost:8080/products/{id}/delete
+     */
+
+    /*
+    GET http://localhost:8080/products/delete/all
+     */
 }
